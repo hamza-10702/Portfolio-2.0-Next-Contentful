04e94e9bcacacdf725be9e4f6a2db35fcd86f613
Hamza <hamzasiddiq755@gmail.com>
2023-08-27 10:20:29 +0500

    card witdth set

--- components/project/ProjectCard.js
+++ components/project/ProjectCard.js
@@ -35,7 +35,7 @@ const ProjectsCards = ({ item }) => {
       <div className="w-full mt-5 flex flex-col  gap-6">
         <div>
           <div className="flex items-center justify-between">
-            <h3 className="text-base uppercase text-designColor font-normal">
+            <h3 className="text-base max-w-[70%] uppercase text-designColor font-normal">
               {item?.fields?.name}
             </h3>
             <div className="flex gap-2">
